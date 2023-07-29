7c03a07ca7f3005e71a3acf2693066c829422557
水木清华 <admin@4u4v.net>
2023-07-29 15:24:31 +0800

    update info

--- config/index.ts
+++ config/index.ts
@@ -3,10 +3,10 @@ export const APP_ID = 'b2bbbc1f-b382-429f-8bce-8d2778595fd0'
 export const API_KEY = 'app-DYTG15eTLRxw2UoKGq4XmjuD'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: '水木心理咨询中心',
+  description: '在心理咨询师的帮助下，慢慢走进自己的心灵深处，共同找出解决问题的方法，增强面对困难的信心，提高解决问题的能力，最终达到“助人自助”的目的，以一种健康的心理、良好的心境、积极的心态面对学习和生活。',
   copyright: '4u4v.com',
-  privacy_policy: 'privacy/policy',
+  privacy_policy: 'https://langgenius.ai/privacy-policy',
   default_language: 'zh-Hans',
 }
 
