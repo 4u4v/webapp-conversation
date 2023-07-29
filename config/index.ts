3ba422c102d984cf3b65647bd8f5f40fdb1eca56
水木清华 <admin@4u4v.net>
2023-07-29 11:12:02 +0800

    update API_KEY

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = ''
-export const API_KEY = ''
+export const APP_ID = 'b2bbbc1f-b382-429f-8bce-8d2778595fd0'
+export const API_KEY = 'app-DYTG15eTLRxw2UoKGq4XmjuD'
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
