149fdd734dd672ee60e8a1caff40cd77f56a5185
4u4v <admin@4u4v.net>
2024-03-23 08:26:23 +0800

    update

--- config/index.ts
+++ config/index.ts
@@ -6,12 +6,12 @@ export const APP_INFO: AppInfo = {
   title: '水木心理咨询中心',
   description: '在心理咨询师的帮助下，慢慢走进自己的心灵深处，共同找出解决问题的方法，增强面对困难的信心，提高解决问题的能力，最终达到“助人自助”的目的，以一种健康的心理、良好的心境、积极的心态面对学习和生活。',
   copyright: '4u4v.com',
-  privacy_policy: 'https://langgenius.ai/privacy-policy',
+  privacy_policy: 'https://www.4u4v.com/privacy-policy',
   default_language: 'zh-Hans',
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = 'I want you to act as a psychological consultant.'
 
 export const API_PREFIX = '/api'
 
